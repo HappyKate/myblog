6cc032a129e34a36951ca96a18f0985f20bee3e2
HappyKate <zlaaayya@gmail.com>
2019-12-10 18:25:26 +0500

    *

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'about' => 'pages'
-  root 'posts#index'
-  resource :posts
+  get 'about' => 'pages', as: 'about'
+  root 'posts#index', as: 'home'
+  resources :posts do
+    resources :comments
+  end
 end
